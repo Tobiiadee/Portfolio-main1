c259788f20fabd381d24426fa5301375daf1bc50
Tobiiadee <oluwatobi121@gmail.com>
2024-07-26 13:56:05 +0100

    "Fixing log errors"

---
+++ lib/utils/auth-options.ts
@@ -0,0 +1,38 @@
+/** @format */
+
+import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebaseConfig";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+
+    Credentials({
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials): Promise<any> {
+        return signInWithEmailAndPassword(
+          auth,
+          (credentials as any).email || "",
+          (credentials as any).password || ""
+        )
+          .then((userCredentials) => {
+            if (userCredentials.user) {
+              return userCredentials.user;
+            }
+            return null;
+          })
+          .catch((err) => console.log(err));
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET!,
+  pages: {
+    signIn: "/sign-in",
+  },
+};
