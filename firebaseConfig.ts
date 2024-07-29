022c2a081ced2a09421dff8fe8a913573c6d6287
Tobiiadee <oluwatobi121@gmail.com>
2024-07-29 19:12:50 +0100

    "fixed env file"

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -11,7 +11,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "project1-0-83d5d.firebaseapp.com",
   databaseURL: "https://project1-0-83d5d-default-rtdb.firebaseio.com",
   projectId: "project1-0-83d5d",
